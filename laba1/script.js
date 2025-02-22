82770daf664b755cf315cf5bc07a11a658ff2306
Kostia Semkiv <semksvk@gmail.com>
2025-02-22 14:26:12 +0200

    fin1

--- laba1/script.js
+++ laba1/script.js
@@ -1,56 +1,43 @@
-function round2(number: number): number {
-    const roundedNumber = number.toFixed(2);
+function round2(number) {
+    var roundedNumber = number.toFixed(2);
     return parseFloat(roundedNumber);
 }
-
-type TriangleType = "leg" | "hypotenuse" | "adjacent angle" | "opposite angle" | "angle";
-
-function triangle(value1: number, type1: TriangleType, value2: number, type2: TriangleType): string {
+function triangle(value1, type1, value2, type2) {
     // Перевірка на некоректність уведених значень
     if (value1 <= 0.01 || value2 <= 0.01) {
         return "failed: value must be positive or bigger";
     }
-
-    const validTypes: TriangleType[] = [
+    var validTypes = [
         "leg",
         "hypotenuse",
         "adjacent angle",
         "opposite angle",
         "angle",
     ];
-    const anglesList: TriangleType[] = ["adjacent angle", "opposite angle", "angle"];
-    
+    var anglesList = ["adjacent angle", "opposite angle", "angle"];
     if (!validTypes.includes(type1) || !validTypes.includes(type2)) {
         return "failed: input types error";
     }
-
-    if (
-        (type1 === "hypotenuse" && type2 === "leg") ||
-        (type1 === "leg" && type2 === "hypotenuse")
-    ) {
+    if ((type1 === "hypotenuse" && type2 === "leg") ||
+        (type1 === "leg" && type2 === "hypotenuse")) {
         if (value1 === value2) {
             return "failed: hypotenuse cannot be equal to leg";
         }
     }
-
     if (anglesList.includes(type1)) {
         if (value1 < 0 || value1 > 90) {
-            return `failed: incorrect angle ${type1}`;
+            return "failed: incorrect angle ".concat(type1);
         }
     }
-
     if (anglesList.includes(type2)) {
         if (value2 < 0 || value2 > 90) {
-            return `failed: incorrect angle ${type2}`;
+            return "failed: incorrect angle ".concat(type2);
         }
     }
-
     if (type1 === "hypotenuse" && type2 === "hypotenuse") {
         return "failed: 2 hypotenuses in triangle";
     }
-
-    let a: number = 0, b: number = 0, c: number = 0, alpha: number = 0, beta: number = 0;
-
+    var a = 0, b = 0, c = 0, alpha = 0, beta = 0;
     switch (type1) {
         case "leg":
             {
@@ -58,15 +45,16 @@ function triangle(value1: number, type1: TriangleType, value2: number, type2: Tr
                 switch (type2) {
                     case "leg": {
                         b = value2;
-                        c = Math.sqrt(a ** 2 + b ** 2);
+                        c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
                         alpha = Math.atan(a / b) * (180 / Math.PI);
                         beta = 90 - alpha;
                         break;
                     }
                     case "hypotenuse": {
-                        if (a > value2) return "failed: hypotenuse smaller than leg";
+                        if (a > value2)
+                            return "failed: hypotenuse smaller than leg";
                         c = value2;
-                        b = Math.sqrt(c ** 2 - a ** 2);
+                        b = Math.sqrt(Math.pow(c, 2) - Math.pow(a, 2));
                         alpha = Math.atan(a / b) * (180 / Math.PI);
                         beta = 90 - alpha;
                         break;
@@ -74,14 +62,14 @@ function triangle(value1: number, type1: TriangleType, value2: number, type2: Tr
                     case "adjacent angle": {
                         alpha = value2;
                         c = a / Math.cos(alpha * (Math.PI / 180));
-                        b = Math.sqrt(c ** 2 - a ** 2);
+                        b = Math.sqrt(Math.pow(c, 2) - Math.pow(a, 2));
                         beta = 90 - alpha;
                         break;
                     }
                     case "opposite angle": {
                         beta = value2;
                         c = a / Math.sin(beta * (Math.PI / 180));
-                        b = Math.sqrt(c ** 2 - a ** 2);
+                        b = Math.sqrt(Math.pow(c, 2) - Math.pow(a, 2));
                         alpha = 90 - beta;
                         break;
                     }
@@ -97,7 +85,7 @@ function triangle(value1: number, type1: TriangleType, value2: number, type2: Tr
                             return "failed: hypotenuse smaller than leg";
                         }
                         a = value2;
-                        b = Math.sqrt(c ** 2 - a ** 2);
+                        b = Math.sqrt(Math.pow(c, 2) - Math.pow(a, 2));
                         alpha = Math.atan(a / b) * (180 / Math.PI);
                         beta = 90 - alpha;
                         break;
@@ -106,7 +94,7 @@ function triangle(value1: number, type1: TriangleType, value2: number, type2: Tr
                         alpha = value2;
                         beta = 90 - alpha;
                         b = c * Math.sin(beta * (Math.PI / 180));
-                        a = Math.sqrt(c ** 2 - b ** 2);
+                        a = Math.sqrt(Math.pow(c, 2) - Math.pow(b, 2));
                         break;
                     }
                 }
@@ -120,13 +108,13 @@ function triangle(value1: number, type1: TriangleType, value2: number, type2: Tr
                     case "leg": {
                         a = value2;
                         c = a / Math.cos(alpha * (Math.PI / 180));
-                        b = Math.sqrt(c ** 2 - a ** 2);
+                        b = Math.sqrt(Math.pow(c, 2) - Math.pow(a, 2));
                         break;
                     }
                     case "hypotenuse": {
                         c = value2;
                         b = c * Math.sin(beta * (Math.PI / 180));
-                        a = Math.sqrt(c ** 2 - b ** 2);
+                        a = Math.sqrt(Math.pow(c, 2) - Math.pow(b, 2));
                         break;
                     }
                 }
@@ -140,13 +128,13 @@ function triangle(value1: number, type1: TriangleType, value2: number, type2: Tr
                     case "leg": {
                         a = value2;
                         c = a / Math.cos(alpha * (Math.PI / 180));
-                        b = Math.sqrt(c ** 2 - a ** 2);
+                        b = Math.sqrt(Math.pow(c, 2) - Math.pow(a, 2));
                         break;
                     }
                     case "hypotenuse": {
                         c = value2;
                         b = c * Math.sin(beta * (Math.PI / 180));
-                        a = Math.sqrt(c ** 2 - b ** 2);
+                        a = Math.sqrt(Math.pow(c, 2) - Math.pow(b, 2));
                         break;
                     }
                 }
@@ -159,21 +147,16 @@ function triangle(value1: number, type1: TriangleType, value2: number, type2: Tr
                     case "hypotenuse":
                         c = value2;
                         b = c * Math.sin(beta * (Math.PI / 180));
-                        a = Math.sqrt(c ** 2 - b ** 2);
+                        a = Math.sqrt(Math.pow(c, 2) - Math.pow(b, 2));
                         alpha = 90 - beta;
                         break;
                 }
             }
             break;
     }
-
     if (beta <= 0.01 || alpha <= 0.01) {
         return "failed: after calculation angle smaller than or equal to zero";
     }
-
-    console.log(
-        `a = ${round2(a)}\nb = ${round2(b)}\nc = ${round2(c)}\nalpha = ${round2(alpha)}°\nbeta = ${round2(beta)}°`
-    );
-
+    console.log("a = ".concat(round2(a), "\nb = ").concat(round2(b), "\nc = ").concat(round2(c), "\nalpha = ").concat(round2(alpha), "\u00B0\nbeta = ").concat(round2(beta), "\u00B0"));
     return "success";
 }
